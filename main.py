145052369dfbf3d80ee8b1b01f9ea98ef2558fc7
mesejo <mesejoleon@gmail.com>
2022-09-04 21:27:48 +0200

    add github action

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Welcome to the sortsearch's Film Catalog", "version": "0.0.3"}
+    return {"message": "Welcome to the sortsearch's Film Catalog", "version": "0.0.4"}
